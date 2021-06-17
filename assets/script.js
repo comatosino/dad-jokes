a61e3044fb6f0215704c670c09e381c050dcee9a
Robert Adams <adamsiii.robert@gmail.com>
2021-06-17 16:08:42 -0700

    added comments

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,19 @@ const jokeBtn = document.getElementById("get-joke");
 const jokeBox = document.getElementById("joke-box");
 const beardBox = document.getElementById("beard-box");
 
+// fetch a random joke from https://icanhazdadjoke.com/
 const getJoke = () => {
+
+    // add context to request
+    // TODO: add user agent
     const init = {
         method: 'GET',
         headers: {
           'Accept': 'application/json'
         },
-      };
+    };
+
+    // fetch a random joke and write to page
     fetch("https://icanhazdadjoke.com", init)
     .then(response => response.json())
     .then(data => jokeBox.textContent = data.joke);
