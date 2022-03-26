652be4e5aaba2cc1851d7691e2b4113483b18a5d
Robert Adams <adamsiii.robert@gmail.com>
2022-03-26 09:21:25 -0700

    wrapped app with ThemeProvider

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/";
 
-import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { ThemeProvider } from "@emotion/react";
+import theme from "./themes/theme";
+
+console.log("Thanks to https://icanhazdadjoke.com for the dad jokes!");
+console.log("Thanks to placebeard.it for the beard images!");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
