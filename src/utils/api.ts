52ba6031a7b88f49c5b65d9c8a4bd4d1190cb743
Robert Adams <adamsiii.robert@gmail.com>
2022-08-09 12:28:57 -0700

    prettier

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,21 +1,21 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { JokeResponse } from "../models/JokeResponse";
+import axios, { AxiosRequestConfig } from 'axios';
+import { JokeResponse } from '../models/JokeResponse';
 
 export default class API {
   static async getJoke(): Promise<string | undefined> {
-    const url = "https://icanhazdadjoke.com";
+    const url = 'https://icanhazdadjoke.com';
     const reqConfig: AxiosRequestConfig = {
-      method: "GET",
+      method: 'GET',
       headers: {
-        Accept: "application/json",
+        Accept: 'application/json',
       },
     };
     try {
       const response = await axios.get<JokeResponse>(url, reqConfig);
       if (response.data.status !== 200) {
-        throw new Error("Uh-oh. Something went wrong while fetching...");
+        throw new Error('Uh-oh. Something went wrong while fetching...');
       }
-      if (!response.data.joke) return "Could not find a joke :(";
+      if (!response.data.joke) return 'Could not find a joke :(';
       return response.data.joke;
     } catch (err) {
       console.error(err);
