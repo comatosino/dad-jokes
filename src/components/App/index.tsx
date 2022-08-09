84f2bb4eb563f42b373f70be70494f6591c7c6a4
Robert Adams <adamsiii.robert@gmail.com>
2022-08-09 12:28:45 -0700

    added dummy query param in fetch

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from "react";
-import API from "../../utils/api";
+import { useState, useEffect } from 'react';
+import API from '../../utils/api';
 
 import {
   Box,
@@ -9,8 +9,10 @@ import {
   Skeleton,
   Stack,
   Typography,
-} from "@mui/material";
-import theme from "../../themes/theme";
+} from '@mui/material';
+import theme from '../../themes/theme';
+
+const IMG_SIZE = 350;
 
 const App: React.FC = (): JSX.Element => {
   const [bothLoaded, setBothLoaded] = useState<boolean>(false);
@@ -24,18 +26,22 @@ const App: React.FC = (): JSX.Element => {
   }, []);
 
   const getBeard = () => {
-    setBeardSrc(() => "");
+    setBeardSrc(() => '');
     setBeardReady(false);
     setTimeout(() => {
-      setBeardSrc(() => "https://placebeard.it/g/350/notag");
+      setBeardSrc(
+        // append a dummy query parameter with Date obj
+        // workaround for Firefox caching imgs from same url
+        () => `https://placebeard.it/g/350/notag?${new Date().getTime()}`
+      );
     }, 100);
   };
 
   const getJoke = async () => {
-    setJoke(() => "");
+    setJoke(() => '');
     const nextJoke = await API.getJoke();
     if (nextJoke) setJoke(nextJoke);
-    else setJoke("Could not find a joke!");
+    else setJoke('Could not find a joke!');
   };
 
   const getBeardJoke = () => {
@@ -50,27 +56,28 @@ const App: React.FC = (): JSX.Element => {
   };
 
   return (
-    <Container maxWidth="xs">
-      <Stack minHeight={625} maxHeight={800} justifyContent="space-between">
-        <Stack alignItems={"center"}>
+    <Container maxWidth='xs'>
+      <Stack minHeight={625} maxHeight={800} justifyContent='space-between'>
+        <Stack alignItems={'center'}>
           <Box height={350} width={350}>
             {!beardReady && (
-              <Box position={"absolute"}>
-                <Skeleton variant="rectangular" height={350} width={350} />
+              <Box position={'absolute'}>
+                <Skeleton variant='rectangular' height={350} width={350} />
               </Box>
             )}
             {beardSrc && (
               <img
+                id='image'
                 height={350}
                 width={350}
                 style={
                   beardReady
-                    ? { display: "block", borderRadius: 5 }
-                    : { display: "none" }
+                    ? { display: 'block', borderRadius: 5 }
+                    : { display: 'none' }
                 }
                 src={beardSrc}
-                alt="bearded person courtesy of https://placebeard.it/"
-                onLoad={() => update()}
+                alt='bearded person courtesy of https://placebeard.it/'
+                onLoad={update}
               />
             )}
           </Box>
@@ -78,17 +85,17 @@ const App: React.FC = (): JSX.Element => {
           <Box
             width={300}
             marginTop={2}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
           >
             {bothLoaded && joke ? (
               <Typography color={theme.palette.text.primary}>{joke}</Typography>
             ) : (
-              <Box width="100%">
-                <Skeleton variant="text" />
-                <Skeleton variant="text" />
-                <Skeleton variant="text" />
+              <Box width='100%'>
+                <Skeleton variant='text' />
+                <Skeleton variant='text' />
+                <Skeleton variant='text' />
               </Box>
             )}
           </Box>
@@ -96,17 +103,17 @@ const App: React.FC = (): JSX.Element => {
 
         <Grid container spacing={2}>
           <Grid item xs={6}>
-            <Button fullWidth onClick={() => getBeard()} variant="outlined">
+            <Button fullWidth onClick={() => getBeard()} variant='outlined'>
               new beard
             </Button>
           </Grid>
           <Grid item xs={6}>
-            <Button fullWidth onClick={() => getJoke()} variant="outlined">
+            <Button fullWidth onClick={() => getJoke()} variant='outlined'>
               new joke
             </Button>
           </Grid>
           <Grid item xs={12}>
-            <Button fullWidth onClick={() => getBeardJoke()} variant="outlined">
+            <Button fullWidth onClick={() => getBeardJoke()} variant='outlined'>
               new beard and dad joke
             </Button>
           </Grid>
