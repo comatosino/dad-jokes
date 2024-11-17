6f61b56672ceda9949620cac97598ad636bd20f6
Robert Adams <r0b4dams@gmail.com>
2024-11-16 20:33:18 -0700

    rm v1

--- src/models/JokeResponse.ts
+++
@@ -1,4 +0,0 @@
-export type JokeResponse = {
-  joke: string;
-  status: number;
-};
--- src/themes/theme.ts
+++
@@ -1,9 +0,0 @@
-import createTheme from "@mui/material/styles/createTheme";
-
-const theme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
-
-export default theme;
